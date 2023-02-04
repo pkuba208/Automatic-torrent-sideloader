a70fd0a46e4755ea9f820068a9ff9e34ea0b2291
pkuba208 <83948992+pkuba208@users.noreply.github.com>
2023-02-04 19:21:38 +0100

    Update and rename brewinstall.sh to setup.sh

--- macos-beta/setup.sh
+++ macos-beta/setup.sh
@@ -3,6 +3,7 @@ xcode-select --install
 curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 /bin/bash install.sh
 brew install aria2
+brew install git
 brew install --cask android-platform-tools
 mkdir quest2downloads
 echo DONE
