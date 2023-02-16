2302ed573dd5714fa95b2a34a67ed7acb41f6a2c
pkuba208 <83948992+pkuba208@users.noreply.github.com>
2023-02-16 21:57:29 +0100

    Update setup.sh

--- macos-beta/setup.sh
+++ macos-beta/setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 xcode-select --install
+echo "Press any key to continue..."
+read -n 1 -s -r
+echo "Resuming script..."
 curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 /bin/bash install.sh
+echo "Press any key to continue..."
+read -n 1 -s -r
+echo "Resuming script..."
+brew install aria2
+brew install git
+brew install --cask android-platform-tools
+mkdir quest2downloads
 echo DONE
